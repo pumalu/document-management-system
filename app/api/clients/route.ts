28754b445bf64c10202eb6795433f87ba066968d
pumalu <lifemeinpassion@gmail.com>
2025-04-05 03:06:21 +0530

    Update route.ts

--- app/api/clients/route.ts
+++ app/api/clients/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]/route"
 import { connectToDatabase } from "@/lib/mongodb"
-import { hash } from "bcrypt"
+import { hash } from "bcryptjs"
 
 export async function GET(request) {
   try {
