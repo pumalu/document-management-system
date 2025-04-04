2aca8011ec6d9b30a82d3df1fffd15d3715770d9
pumalu <lifemeinpassion@gmail.com>
2025-04-05 03:06:01 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
-import { compare } from "bcrypt"
+import { compare } from "bcryptjs"
 import { connectToDatabase } from "@/lib/mongodb"
 
 export const authOptions = {
