77125bfaf9a0fc064996e561084ea5b43b38c980
pumalu <lifemeinpassion@gmail.com>
2025-04-05 04:15:09 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,26 @@
 import mongoose from "mongoose";
 
-export const connectToDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
-  return mongoose.connect(process.env.MONGODB_URI!, {
-    dbName: "document-management", // isko tu apne hisaab se change kar sakta hai
-  });
-};
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
